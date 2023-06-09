1bcdff2b03f05760004ed4be9cb395fe71c3f953
Ari Kurniawan <arikurnia2601@gmail.com>
2023-06-09 18:55:19 +0700

    memisahkan navbar menjadi sebuah component.

---
+++ components/navbar.php
@@ -0,0 +1,27 @@
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="navbar">
+            <div class="container-fluid">
+                <a class="navbar-brand company-title" href="#">
+                    <img src="assets/images/logo-dianterin.png" alt="Logo" width="50" height="34" class="d-inline-block align-text-top" />
+                    diAnterin
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto border border-danger">
+                        <li class="nav-item mx-lg-3">
+                            <a class="nav-link" href="/index.php">Home</a>
+                        </li>
+                        <li class="nav-item mx-lg-3">
+                            <a class="nav-link" href="/products.php">Product</a>
+                        </li>
+                        <li class="nav-item mx-lg-3">
+                            <a class="nav-link" href="/about.php">About Us</a>
+                        </li>
+                    </ul>
+                    <li class="nav-item mb-2 border border-danger">
+                        <a class="btn btn-warning" href="/admin/index.php">Login</a>
+                    </li>
+                </div>
+            </div>
+        </nav>
