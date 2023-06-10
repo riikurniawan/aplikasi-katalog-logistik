a1dad91400a34f34f0934b09702505e811285ff7
Ari Kurniawan <arikurnia2601@gmail.com>
2023-06-11 01:14:48 +0700

    updated

--- admin/login.php
+++ admin/login.php
@@ -1,23 +1,22 @@
 <?php
 session_start();
 
-spl_autoload_register(function() {
-    require_once "../config/Functions.php";
+spl_autoload_register(function () {
+    require_once "../Config/Functions.php";
 });
 
-
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     $func = new Functions();
     $result = $func->login($username, $password);
-    if($result["success"]) {
+    if ($result["success"]) {
         $_SESSION['logged_in'] = $username;
         echo json_encode(array('success' => true));
     } else {
         echo json_encode(array('errors' => $result["fail"]));
     }
 } else {
-    header("Location: index.php", true, 302);
+    header("Location: index.php");
 }
